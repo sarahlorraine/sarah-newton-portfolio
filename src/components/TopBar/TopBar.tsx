0e1f61ac6332989d46a150e53469c1532c8ca100
Sarah Newton <sarahnewton@Sarahs-MacBook.lan>
2022-01-20 11:51:49 +0100

    Style fixes to page layoutand top menu

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -13,23 +13,25 @@ const TopBar = () => {
 
   return (
     <div className={mergeClassNames([mobileNavActive && "topBarWrapperActive", "topBarWrapper"])}>
-     <Link to="/" className={"logo"}>arah Newton</Link>
-     <div className="navigationMobile">
-       <button 
-          className={mergeClassNames([mobileNavActive && "burgerBtnActive", "headerBurgerBtn"])} 
-          onClick={() => setMobileNavActive(!mobileNavActive)}
-        >
-         <div className="burgerBox">
-            <div className="topBun"></div>
-            <div className="bottomBun"></div>
-          </div>
-        </button>
-     </div>
-     <div className={mergeClassNames([mobileNavActive && "mobileShow", "navigation"])}>
-        <Link to="/design-portfolio">Design Portfolio</Link>
-        <a target="_blank" href="https://www.linkedin.com/in/sarah-newton-66077553/">LinkedIn</a>
-        <a target="_blank" href="https://github.com/sarahlorraine">GitHub</a>
-        <Button type="download" className="cvBtn" href={cv}>CV</Button>
+      <div className="innerContent">
+      <Link to="/" className={"logo"}>arah Newton</Link>
+      <div className="navigationMobile">
+        <button 
+            className={mergeClassNames([mobileNavActive && "burgerBtnActive", "headerBurgerBtn"])} 
+            onClick={() => setMobileNavActive(!mobileNavActive)}
+          >
+          <div className="burgerBox">
+              <div className="topBun"></div>
+              <div className="bottomBun"></div>
+            </div>
+          </button>
+      </div>
+      <div className={mergeClassNames([mobileNavActive && "mobileShow", "navigation"])}>
+          <Link to="/design-portfolio">Design Portfolio</Link>
+          <a target="_blank" href="https://www.linkedin.com/in/sarah-newton-66077553/">LinkedIn</a>
+          <a target="_blank" href="https://github.com/sarahlorraine">GitHub</a>
+          <Button type="download" className="cvBtn" href={cv}>CV</Button>
+      </div>
      </div>
     </div>
   );
